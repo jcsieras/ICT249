86166e4a5dba41f709a923f455afdba0211544ef
Joseph C. Sieras <jcsieras@gmail.com>
2013-06-28 17:24:19 +0800

    Helloworld

---
+++ Helloworld/src/com/example/helloworld/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.helloworld;
+
+import org.apache.cordova.DroidGap;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends DroidGap {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.loadUrl("file:///android_asset/www/index.html");
+		//setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
